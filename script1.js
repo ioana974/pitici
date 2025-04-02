2c5f28bcfa4ed40be90fe26426f610a351654ca6
ioana974 <ioana.elena.turda@sincaibm.ro>
2025-04-02 16:35:10 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+﻿// Obține elementele pop-up
+const openRegisterPopup = document.getElementById('open-register-popup');
+const registerPopup = document.getElementById('register-popup');
+const closeRegisterPopup = document.getElementById('close-register-popup');
+
+if (openRegisterPopup && registerPopup && closeRegisterPopup) {
+    // Deschide pop-up-ul la click pe buton
+    openRegisterPopup.addEventListener('click', function() {
+        registerPopup.style.display = 'block';
+    });
+
+    // Închide pop-up-ul la click pe "x"
+    closeRegisterPopup.addEventListener('click', function() {
+        registerPopup.style.display = 'none';
+    });
+
+    // Închide pop-up-ul dacă utilizatorul face clic în afara acestuia
+    window.addEventListener('click', function(event) {
+        if (event.target === registerPopup) {
+            registerPopup.style.display = 'none';
+        }
+    });
+}
